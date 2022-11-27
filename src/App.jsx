4cb0d6ef95ba8eb1279fa7900a7310e7e96a048e
PawelDuplaga <pawelduplagait@gmail.com>
2022-11-27 03:18:26 +0100

    multi cards added

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,27 @@ import './App.css'
 import './components/Navbar'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection/HeroSection'
-import collage_image from '../public/assets/Group 77.png'
 import Card from './components/Card/Card'
+import data from './data.js'
 
 function App() {
+  console.log(data)
+const cards = data.map(item => {
+  return (
+    <Card
+    key = {item.id}
+    img = {item.coverImg}
+    rating = {item.stats.rating}
+    reviews_count = {item.stats.reviewCount}
+    country = {item.location}
+    description = {item.title}
+    card_price = {item.price}
+    openSpots ={item.openSpots}    
+    />
+  )
+})
+
+
  return (
   <div className='main'>
     <Navbar/>
@@ -15,13 +32,10 @@ function App() {
     title = "Be the best version of yourself"
     text = "Join unique interactive activities led by one-of-a-kind hosts—all without leaving home."
     /> */}
-    <Card
-    img = "CardImg.png"
-    reviews_count = '5'
-    country = 'USA'
-    description = 'Life lessons with Katie Zaferes'
-    card_price = 'From $135'
-    />
+    <section className='cards-section'>
+    {cards}
+    </section>
+
   </div>
  )
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,13 +3,15 @@ import './Card.css'
 
 export default function Card(params) {
 
+    params.key
     return(
         <div className="card--container">
+            {params.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
             <img src = {`../../public/assets/${params.img}`} className="card--image" />
             <div className="card--info">
                 <div className="card--reviews-score-country">
                     <img className="card--star-icon" />
-                    <span className="card--reviews-count grey">({params.reviews_count})</span>
+                    <span className="card--reviews-count grey">{params.rating}({params.reviews_count})</span>
                     <span className="grey">•</span>
                     <span className="card--country grey">{params.country}</span>
                 </div>
